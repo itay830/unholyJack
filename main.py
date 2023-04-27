1065eda495a90e0e735784e56ccb8a326ece53a2
Itay <itay.shinderman@gmail.com>
2023-04-27 18:35:47 +0300

    A test for organisation share

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import pygame
 from sys import exit
 
+BLACK, WHITE = (0, 0, 0), (255, 255, 255)
+
+class App:
+    def __init__(self):
+        self.gameState = 'main menu'
+
+
 if __name__ == '__main__':
+    app = App()
     WIDTH, HEIGHT = 1200, 800
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('UnholyJack')
 
     while 1:
-        screen.fill((0, 0, 0))
+        if app.gameState == "main menu":
+            screen.fill(BLACK)
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+
+
 
         pygame.display.update()
 
